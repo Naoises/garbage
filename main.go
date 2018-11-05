af331b57ff093275075c389d323da17448ac88b2
cadent.io <naoise.dunne@chatspace.ai>
2018-11-05 12:12:50 +0000

    commit 1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
+	title := "Jenkins X golang http example may the road rise with you"
 
 	from := ""
 	if r.URL != nil {
